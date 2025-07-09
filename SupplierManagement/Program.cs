04c78e768f79c12fb2c17094ca39c159f8cb2859
yildirim-metin <metin_yildirim@outlook.be>
2025-07-09 14:48:26 +0200

    Move menu testing logic in a TestMenu Class

--- SupplierManagement/Program.cs
+++ SupplierManagement/Program.cs
@@ -1,40 +1,5 @@
-﻿using MenuLibrary;
+﻿using SupplierManagement;
 
 Console.WriteLine("Supplier management");
 
-IItem episode1 = new Item("Episode 1");
-Console.WriteLine(episode1.Label);
-
-Menu mainMenu = new("Menu principale");
-
-IAction action = new ActionTest();
-if (episode1 is Item itemTest)
-{
-    itemTest.Action = action;
-    itemTest.Start();
-
-    mainMenu.AddItem('a', itemTest);
-}
-
-Item episode2 = new("Episode 2");
-Item episode3 = new("Episode 3");
-Item episode4 = new("Episode 4");
-
-Menu secondMenu = new("Menu secondaire");
-secondMenu.AddItem('a', episode1);
-secondMenu.AddItem('b', episode2);
-
-mainMenu.AddItem('b', episode2);
-mainMenu.AddItem('c', episode3);
-mainMenu.AddItem('d', episode4);
-mainMenu.AddItem('e', secondMenu);
-
-mainMenu.Start();
-
-Menu menu = new MenuBuilder("test")
-    .Item('a', "test 1", action)
-    .Menu('b', new MenuBuilder("test 2").Item('a', "test 2.1", action).Build())
-    .Item('c', "test 3", action)
-    .Build();
-
-menu.Start();
+TestMenu.Test();
---
+++ SupplierManagement/TestMenu.cs
@@ -0,0 +1,46 @@
+﻿using MenuLibrary;
+
+namespace SupplierManagement;
+
+internal static class TestMenu
+{
+    internal static void Test()
+    {
+        IItem episode1 = new Item("Episode 1");
+        Console.WriteLine(episode1.Label);
+
+        Menu mainMenu = new("Menu principale");
+
+        IAction action = new ActionTest();
+        if (episode1 is Item itemTest)
+        {
+            itemTest.Action = action;
+            itemTest.Start();
+
+            mainMenu.AddItem('a', itemTest);
+        }
+
+        Item episode2 = new("Episode 2");
+        Item episode3 = new("Episode 3");
+        Item episode4 = new("Episode 4");
+
+        Menu secondMenu = new("Menu secondaire");
+        secondMenu.AddItem('a', episode1);
+        secondMenu.AddItem('b', episode2);
+
+        mainMenu.AddItem('b', episode2);
+        mainMenu.AddItem('c', episode3);
+        mainMenu.AddItem('d', episode4);
+        mainMenu.AddItem('e', secondMenu);
+
+        mainMenu.Start();
+
+        Menu menu = new MenuBuilder("test")
+            .Item('a', "test 1", action)
+            .Menu('b', new MenuBuilder("test 2").Item('a', "test 2.1", action).Build())
+            .Item('c', "test 3", action)
+            .Build();
+
+        menu.Start();
+    }
+}
